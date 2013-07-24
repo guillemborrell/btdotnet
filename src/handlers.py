e1e736d625c71c35c17707bff1ddd8d27e6281d1
Guillem Borrell <guillemborrell@gmail.com>
2013-07-24 11:53:57 +0200

    Sanitized the authentication quite a lot. Now it only returns two variables that give enough information to know the status of the user. Everything is handled with oauth.session_auth

--- src/handlers.py
+++ src/handlers.py
@@ -1,1 +1,49 @@
 # Handlers for forms and bets.
+import webapp2
+from webapp2_extras import sessions
+from oauth import session_auth
+
+
+class BaseHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        self.session_store = sessions.get_store(request=self.request)
+        
+        try:
+            webapp2.RequestHandler.dispatch(self)
+        
+        finally:
+            self.session_store.save_sessions(self.response)
+        
+    @webapp2.cached_property
+    def session(self):
+        return self.session_store.get_session()
+
+
+class MainPage(BaseHandler):     
+    def get(self):
+        auth, var = session_auth(
+            self.session,
+            self.request, 
+            'http://betweetdotnet.appspot.com')
+
+
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write('''
+        <html>
+        <body>
+        <p>Hello, World!</p>
+        ''')
+
+        if auth == 'ANONYMOUS':
+            self.response.out.write('<p><a href="{}">Log in</a></p>'.format(var))
+        elif auth == 'LOGGED_IN':
+            self.response.out.write('<p>Logged in as {}</p>'.format(var['screen_name']))
+        else:
+            self.response.out.write('<p>Something wrong happened {}</p>'.format(auth))
+        self.response.out.write('''
+        </body>
+        </html>''')
+
+        
+    def post(self):
+        print self.request.get('arg')
--- src/oauth.py
+++ src/oauth.py
@@ -6,13 +6,74 @@ Created on Jul 20, 2013
 from twython import Twython
 from google.appengine.ext import ndb
 
+APP_KEY = 'dmyD2CHHePKQ6M7bQJg3wQ'
+APP_SECRET = 'tAsyLFGu9GIuYluUbNjqmv29Cg0VifK5w8yIKFrmo'
+
+
+def session_auth(session,request,this_url):
+    #Function that manages the session status.
+    if session.get('key'):
+        #If the cookie is found, get the key and look for the session data in the database
+        session_data = ndb.Key(urlsafe = session.get('key')).get()
+            
+        #If the page is visited from the twitter authentication callback, it will receive the oauth_verifier parameter
+        if request.get('oauth_verifier'):
+            oauth_verifier = request.get('oauth_verifier')
+            oauth_token = request.get('oauth_token')
+            twitter = Twython(APP_KEY, APP_SECRET,
+                              session_data.oauth_token,
+                              session_data.oauth_token_secret)
+        
+                #The twitter authentication returns oauth_token, and it is compared with the original token
+            if session_data.oauth_token == oauth_token:
+                final_step = twitter.get_authorized_tokens(oauth_verifier)
+                session_data.oauth_token = final_step['oauth_token']
+                session_data.oauth_token_secret = final_step['oauth_token_secret']
+                session_data.oauth_verifier = oauth_verifier
+                key = session_data.put()
+                session['key'] = key.urlsafe()
+                auth = Twython(APP_KEY, APP_SECRET,
+                               session_data.oauth_token,
+                               session_data.oauth_token_secret)
+                credentials = auth.verify_credentials()
+                return 'LOGGED_IN', credentials
+                    
+            # If the tokens are not equal, an exception is raised.
+            else:
+                return 'ERROR', None
+        
+        elif session_data.oauth_verifier:
+            auth = Twython(APP_KEY, APP_SECRET,
+            session_data.oauth_token,
+            session_data.oauth_token_secret)
+            credentials = auth.verify_credentials()
+            return 'LOGGED_IN', credentials
+        
+        else:
+            twitter  = Twython(APP_KEY, APP_SECRET)
+            auth = twitter.get_authentication_tokens(this_url)
+            session_data = Session(oauth_token = auth['oauth_token'],
+                                   oauth_token_secret = auth['oauth_token_secret'])
+            key = session_data.put()
+            session['key'] = key.urlsafe()
+            return 'ANONYMOUS', auth['auth_url']
+        
+    else:
+        twitter  = Twython(APP_KEY, APP_SECRET)
+        auth = twitter.get_authentication_tokens(this_url)
+        session_data = Session(oauth_token = auth['oauth_token'],
+                               oauth_token_secret = auth['oauth_token_secret'])
+        key = session_data.put()
+        session['key'] = key.urlsafe()
+        return 'ANONYMOUS', auth['auth_url']
+    
 
 class Session(ndb.Model):
     oauth_token = ndb.StringProperty()
     oauth_token_secret = ndb.StringProperty()
     oauth_verifier = ndb.StringProperty()
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
-    
+    username = ndb.StringProperty()
     
 
 class OAuth(object):
